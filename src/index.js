9082767e961bcbbf37df19da3fdaed57af8dcf55
Daniel Kolas <daniel.piotr.kolas@gmail.com>
2023-12-28 19:24:29 +0100

    added local storage for bes scores

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import Phaser from "phaser";
+import PlayScene from "./scenes/PlayScene";
+
+// VARIABLES 
+const WIDTH = 800 ;
+const HEIGHT = 600 ;
+const BIRD_POSITION = {x: WIDTH / 10, y: HEIGHT / 2};
+
+const SHARED_CONFIG = {
+    width: WIDTH,
+    height: HEIGHT,
+    startPosition: BIRD_POSITION
+}
+
+// CONFIG 
+const config = {
+    type: Phaser.AUTO,
+    ...SHARED_CONFIG,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+        }
+    },
+    scene: [new PlayScene(SHARED_CONFIG)]
+}
+
+new Phaser.Game(config);
